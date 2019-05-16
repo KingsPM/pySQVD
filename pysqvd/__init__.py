198a6564cd71f7763789192f9fdc1e81a2567ac5
David Brawand <david.brawand@gmail.com>
2019-05-16 19:23:42 +0100

    minor

--- pysqvd/__init__.py
+++ pysqvd/__init__.py
@@ -16,10 +16,13 @@ __license__ = "MIT"
 __maintainer__ = "David Brawand"
 __email__ = "dbrawand@nhs.net"
 
+
 class ApiError(Exception):
     """Exception raised for errors in or while using the SQVD API"""
+
     def __init__(self, message):
         super(ApiError, self).__init__(message)
+
 
 def safeKeys(iterable):
     """recursively substitutes $./ characters from json keys for MongoDB
@@ -30,7 +33,7 @@ def safeKeys(iterable):
     """
     if type(iterable) is dict:
         for key in iterable.keys():
-            newKey = key.replace('.','-').replace('$','£')
+            newKey = key.replace('.', '-').replace('$', '£')
             iterable[newKey] = iterable.pop(key)
             if type(iterable[newKey]) is dict or type(iterable[newKey]) is list:
                 iterable[newKey] = safeKeys(iterable[newKey])
@@ -39,6 +42,7 @@ def safeKeys(iterable):
             item = safeKeys(item)
     return iterable
 
+
 def weekdaysFromNow(days):
     """Returns a datetime object with the number of weekdays added.
 
@@ -46,7 +50,7 @@ def weekdaysFromNow(days):
     :type days: int.
     :returns:  datetime.
     """
-    startdate = datetime.now().replace(hour=0,minute=0,second=0,microsecond=0)
+    startdate = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     while days > 0:
         startdate += timedelta(days=1)
         if startdate.weekday() >= 5:  # next if weekend
@@ -54,9 +58,10 @@ def weekdaysFromNow(days):
         days -= 1
     return startdate
 
+
 class SQVD(object):
 
-    def __init__(self,username,password,host,version='v1'):
+    def __init__(self, username, password, host, version='v1'):
         """creates pySQVD class
 
         :param username: SQVD username.
@@ -69,7 +74,7 @@ class SQVD(object):
         :type version: str.
         """
         self.host = host
-        self.url = 'http://'+"/".join([host,'api',version])
+        self.url = 'http://'+"/".join([host, 'api', version])
         self.session = None
         self.userid = None
         self.username = username
@@ -79,13 +84,13 @@ class SQVD(object):
         assert self.login()
         return self
 
-    def __exit__(self,*args):
+    def __exit__(self, *args):
         self.logout()
 
     def __str__(self):
         return '<SQVD  '+self.username+'@'+self.url+(' authenticated >' if self.session else ' >')
 
-    def login(self,username=None,password=None):
+    def login(self, username=None, password=None):
         """Creates session with authentication headers and sets username and userid
 
         :param username: SQVD username.
@@ -97,7 +102,8 @@ class SQVD(object):
         if username:
             self.username = username
         if password:
-            self.password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+            self.password = hashlib.sha256(
+                password.encode('utf-8')).hexdigest()
 
         r = requests.post(self.url+'/login', data={
             'username': self.username,
@@ -111,7 +117,8 @@ class SQVD(object):
             return
         self.userid = auth['userId']
         self.session = requests.Session()
-        self.session.headers.update({ "X-Auth-Token": auth['authToken'], "X-User-Id": auth['userId'] })
+        self.session.headers.update(
+            {"X-Auth-Token": auth['authToken'], "X-User-Id": auth['userId']})
         return True
 
     def logout(self):
@@ -125,7 +132,7 @@ class SQVD(object):
                 self.userid = None
                 return True
 
-    def _checkResponse(self,response):
+    def _checkResponse(self, response):
         """checks if 200 and API returns success, else prints error message
 
         :param response: the API response object
@@ -147,26 +154,29 @@ class SQVD(object):
         :param data: Form Data
         :type data: dict/string.
         :param json: JSON Data, keys are sanitized for mongoDB
-        :type data: dict/json.
+        :type json: dict/json.
         :returns: response object.
         """
-        baseUrl = [self.url,collection]
+        baseUrl = [self.url, collection]
         if op == 'GET':
             if isinstance(data, string_types):
                 baseUrl.append(data)
             elif isinstance(data, dict):
-                baseUrl[-1] += '?'+'&'.join(map(lambda k: str(k)+'='+str(data[k]),data.keys()))
+                baseUrl[-1] += '?' + \
+                    '&'.join(map(lambda k: str(k)+'=' +
+                                 str(data[k]), data.keys()))
             r = self.session.request(op, '/'.join(baseUrl))
         elif op == 'DELETE':
             baseUrl.append(data)
             r = self.session.request(op, '/'.join(baseUrl))
         elif op == 'POST':
-            r = self.session.request(op, '/'.join(baseUrl), data=data, json=safeKeys(json))
+            r = self.session.request(
+                op, '/'.join(baseUrl), data=data, json=safeKeys(json))
         # check if successful
         if self._checkResponse(r):
             return r.json()
 
-    def createStudy(self,x,find=False):
+    def createStudy(self, x, find=False):
         """Creates a new study and sample if doesnt exist, validates track and panel
 
         :param x: Dictionary with study/sample information.
@@ -180,8 +190,8 @@ class SQVD(object):
 
         newstudy = {
             'study_name': x['study_name'],
-            "subpanels" : x['subpanels'],
-            "group" : x["group"],
+            "subpanels": x['subpanels'],
+            "group": x["group"],
             "createdBy": self.userid,
             'requested': now.isoformat()
         }
@@ -189,9 +199,11 @@ class SQVD(object):
 
         # check panel and subpanels
         try:
-            panel = self.rest('panel',data={k: x[k] for k in ('panel_id', 'panel_version')})
+            panel = self.rest(
+                'panel', data={k: x[k] for k in ('panel_id', 'panel_version')})
             assert len(panel['data']) == 1
-            assert set(x['subpanels']) <= set(map(lambda x: x['subpanel_id'], panel['data'][0]['subpanels']))
+            assert set(x['subpanels']) <= set(
+                map(lambda x: x['subpanel_id'], panel['data'][0]['subpanels']))
             duedate = weekdaysFromNow(int(panel['data'][0]['tat']))
         except AssertionError:
             raise ApiError('panel or subpanels not found')
@@ -203,7 +215,7 @@ class SQVD(object):
             newstudy['reportdue'] = duedate.isoformat()
         # check if track exists
         try:
-            track = self.rest('track',data={'name': x['workflow']})
+            track = self.rest('track', data={'name': x['workflow']})
             assert len(track['data']) == 1
         except AssertionError:
             raise ApiError('Workflow not found')
@@ -214,7 +226,8 @@ class SQVD(object):
 
         # check if study already exists (returns if find enabled)
         try:
-            study = self.rest('study',data={k: x[k] for k in ('study_name', 'group')})['data']
+            study = self.rest(
+                'study', data={k: x[k] for k in ('study_name', 'group')})['data']
             assert len(study) == 0
         except AssertionError:
             if find and len(study) == 1:
@@ -226,7 +239,8 @@ class SQVD(object):
 
         # check sample
         try:
-            sample = self.rest('sample',data={k: x[k] for k in ('sample_id','group')})
+            sample = self.rest(
+                'sample', data={k: x[k] for k in ('sample_id', 'group')})
             assert len(sample['data']) <= 1
         except AssertionError:
             raise ApiError('ambiguous sample name')
@@ -235,20 +249,20 @@ class SQVD(object):
 
         # create sample or get _id
         try:
-            _id = self.rest('sample','POST',data={
-                "group" : x['group'],
-                "sample_id" : x['sample_id'],
-                "received" : now.isoformat(),
-                "bookedBy" : self.userid
+            _id = self.rest('sample', 'POST', data={
+                "group": x['group'],
+                "sample_id": x['sample_id'],
+                "received": now.isoformat(),
+                "bookedBy": self.userid
             })['data']['_id'] if not sample['data'] else sample['data'][0]['_id']
         except:
             raise
         else:
             newstudy["sample_id"] = _id
 
-        return self.rest('study','POST',newstudy)['data']
+        return self.rest('study', 'POST', newstudy)['data']
 
-    def upload(self,files,study_name,parse=True):
+    def upload(self, files, study_name, parse=True):
         """Adds a file to a study (imports VCFs, uploads BEDs)
 
         :param files: file paths
@@ -262,7 +276,7 @@ class SQVD(object):
         :raises: AttributeError, AssertionError, KeyError
         """
         # get study
-        study = self.rest('study',data={'study_name': study_name})
+        study = self.rest('study', data={'study_name': study_name})
         try:
             assert len(study['data']) == 1
         except AssertionError:
@@ -273,89 +287,101 @@ class SQVD(object):
         else:
             results = []
             for fi in files:
-                ## get filename
-                m = re.search(r'\.(.[^\.]+)(\.gz)?$',fi)
+                # get filename
+                m = re.search(r'\.(.[^\.]+)(\.gz)?$', fi)
                 if m:
                     filetype = m.group(1)
-                    if os.path.isfile(fi) and m and filetype in ['vcf','bed','bedgraph','bam','pdf']:
-                        url = '/'.join([self.url, 'study', study['data'][0]['_id'], filetype])
+                    if os.path.isfile(fi) and m and filetype in ['vcf', 'bed', 'bedgraph', 'bam', 'pdf']:
+                        url = '/'.join([self.url, 'study',
+                                        study['data'][0]['_id'], filetype])
                         # set query parameters
-                        url += '?%s' % (urlencode({'filename': fi.split('/')[-1]})) # add filename
-                        url += '&import=true' if parse else '' # import all recognised files
+                        # add filename
+                        url += '?%s' % (
+                            urlencode({'filename': fi.split('/')[-1]}))
+                        url += '&import=true' if parse else ''  # import all recognised files
                         # read file
-                        with open(fi,'rb') as fh:
+                        with open(fi, 'rb') as fh:
                             data = fh.read()
                         # post request
                         r = self.session.request('POST',
-                            url,
-                            headers={'Content-Type': 'application/octet-stream'},
-                            data=data)
+                                                 url,
+                                                 headers={
+                                                     'Content-Type': 'application/octet-stream'},
+                                                 data=data)
                         if self._checkResponse(r):
-                            results.append((fi,r.json()))
+                            results.append((fi, r.json()))
                     else:
-                        print('ERROR: {} is not valid file'.format(os.path.basename(fi)))
+                        print('ERROR: {} is not valid file'.format(
+                            os.path.basename(fi)))
                 else:
-                    print('ERROR: {} is an unsupported format'.format(os.path.basename(fi)))
+                    print('ERROR: {} is an unsupported format'.format(
+                        os.path.basename(fi)))
             return results
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     import sys
-    if (len(sys.argv)<4):
+    if (len(sys.argv) < 4):
         print('test with USERNAME PASSWORD VCF as arguments')
         sys.exit(1)
 
-    sqvd = SQVD(username=sys.argv[1], password=sys.argv[2], host='127.0.0.1:3000')
+    sqvd = SQVD(username=sys.argv[1],
+                password=sys.argv[2], host='127.0.0.1:3000')
     with sqvd:
         # get all studies
         studies = sqvd.rest('study')
-        print('STUDIES:',len(studies['data']),studies['userid'],studies['requested'],str(studies['querytime'])+'ms')
+        print('studies', studies)
+        print('STUDIES:', len(studies['data']), studies['userid'],
+              studies['requested'], str(studies['querytime'])+'ms')
 
         # post study
         data = {
-            "study_name" : "XXXXXX",
-            "sample_id" : "XXXXXX",
-            "panel_id" : "XXXXXX",
-            "subpanels" : [],
-            "group" : "molpath",
-            "requested" : "2018-03-26T08:11:49Z",
-            "reportdue" : "2018-03-28T23:00:00Z",
-            "track_id" : "SOMATIC",
-            "dataset_id" : "XXXXXX"
+            "study_name": "XXXXXX",
+            "sample_id": "XXXXXX",
+            "panel_id": "XXXXXX",
+            "subpanels": [],
+            "group": "molpath",
+            "requested": "2018-03-26T08:11:49Z",
+            "reportdue": "2018-03-28T23:00:00Z",
+            "track_id": "SOMATIC",
+            "dataset_id": "XXXXXX"
         }
-        study = sqvd.rest('study','POST',data)
-        print('CREATED STUDY:',study['data']['_id'])
+        study = sqvd.rest('study', 'POST', data)
+        print('CREATED STUDY:', study['data']['_id'])
 
         # show sample count
-        print("STUDYCOUNT:",len(sqvd.rest('study')['data']))
+        print("STUDYCOUNT:", len(sqvd.rest('study')['data']))
 
         # get document
-        print('GET BY ID:',len(sqvd.rest('study','GET',study['data']['_id'])['data']))
+        print('GET BY ID:', len(
+            sqvd.rest('study', 'GET', study['data']['_id'])['data']))
 
         # with query parameters
-        study = sqvd.rest('study',data={'study_name': study['data']['study_name']})
-        print('GET BY NAME:',len(study['data']))
+        study = sqvd.rest(
+            'study', data={'study_name': study['data']['study_name']})
+        print('GET BY NAME:', len(study['data']))
 
         # delete this study
-        print('DELETE:',len(sqvd.rest('study','DELETE',study['data'][0]['_id'])['data']))
+        print('DELETE:', len(
+            sqvd.rest('study', 'DELETE', study['data'][0]['_id'])['data']))
 
         # create study
         obj = {
             'study_name': 'apitest',
             'sample_id': 'apitest',
-            'panel_id': 'QSPE',
+            'panel_id': 'TEST',
             'panel_version': 1,
-            'workflow': 'dna_somatic',
-            'subpanels': [ 'FOUR', 'FUSION' ],
-            'group': 'haemonc'
+            'workflow': 'somatic',
+            'subpanels': ['FULL'],
+            'group': 'precmed'
         }
         study = sqvd.createStudy(obj)
-        print('CREATED STUDY/SAMPLE:',study['_id'],study['sample_id'])
+        print('CREATED STUDY/SAMPLE:', study['_id'], study['sample_id'])
 
         # test upload
-        uploaded = sqvd.upload(sys.argv[3:],'apitest')
-        print('UPLOADED:',len(uploaded))
+        uploaded = sqvd.upload(sys.argv[3:], 'apitest')
+        print('UPLOADED:', len(uploaded))
 
         # remove study and sample
-        sqvd.rest('study','DELETE',study['_id'])
-        sqvd.rest('sample','DELETE',study['sample_id'])
+        sqvd.rest('study', 'DELETE', study['_id'])
+        sqvd.rest('sample', 'DELETE', study['sample_id'])
