11d4dd1508c73f0f132529124cb9bd2a416c8208
Philip Davidson <phildavidson101@gmail.com>
2022-05-03 12:59:37 +0100

    adding txt and tsv as approved filetypes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pysqvd',
-  version='1.2.4',
+  version='1.2.5-dev',
   description='Python API library for SQVD',
   url='http://github.com/preciserobot/sqvd',
   author='David Brawand',
